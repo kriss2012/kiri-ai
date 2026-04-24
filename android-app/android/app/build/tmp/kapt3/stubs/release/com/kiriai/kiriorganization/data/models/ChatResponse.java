afcbae54477bcbdbb3885d583f02cd9cd3b51976
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-24 21:32:14 +0530

    Update ChatResponse.java

--- android-app/android/app/build/tmp/kapt3/stubs/release/com/kiriai/kiriorganization/data/models/ChatResponse.java
+++ android-app/android/app/build/tmp/kapt3/stubs/release/com/kiriai/kiriorganization/data/models/ChatResponse.java
@@ -1,6 +1,6 @@
 package com.kiriai.kiriorganization.data.models;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u000b\n\u0002\u0010\b\n\u0002\b\u0013\b\u0087\b\u0018\u00002\u00020\u0001BM\u0012\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u0003\u0012\n\b\u0002\u0010\u0004\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\u0007\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\b\u001a\u0004\u0018\u00010\u0001\u0012\n\b\u0002\u0010\t\u001a\u0004\u0018\u00010\u0001\u00a2\u0006\u0002\u0010\nJ\u0010\u0010\u0018\u001a\u0004\u0018\u00010\u0003H\u00c6\u0003\u00a2\u0006\u0002\u0010\u0015J\u000b\u0010\u0019\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001a\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001b\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001c\u001a\u0004\u0018\u00010\u0001H\u00c2\u0003J\u000b\u0010\u001d\u001a\u0004\u0018\u00010\u0001H\u00c2\u0003JV\u0010\u001e\u001a\u00020\u00002\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u00032\n\b\u0002\u0010\u0004\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\u0007\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\b\u001a\u0004\u0018\u00010\u00012\n\b\u0002\u0010\t\u001a\u0004\u0018\u00010\u0001H\u00c6\u0001\u00a2\u0006\u0002\u0010\u001fJ\u0013\u0010 \u001a\u00020\u00032\b\u0010!\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\"\u001a\u00020\u0011H\u00d6\u0001J\t\u0010#\u001a\u00020\u0005H\u00d6\u0001R\u0012\u0010\t\u001a\u0004\u0018\u00010\u00018\u0002X\u0083\u0004\u00a2\u0006\u0002\n\u0000R\u0012\u0010\b\u001a\u0004\u0018\u00010\u00018\u0002X\u0083\u0004\u00a2\u0006\u0002\n\u0000R\u0013\u0010\u0006\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR\u0013\u0010\u0004\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\fR\u0011\u0010\u000e\u001a\u00020\u00058F\u00a2\u0006\u0006\u001a\u0004\b\u000f\u0010\fR\u0011\u0010\u0010\u001a\u00020\u00118F\u00a2\u0006\u0006\u001a\u0004\b\u0012\u0010\u0013R\u0015\u0010\u0002\u001a\u0004\u0018\u00010\u0003\u00a2\u0006\n\n\u0002\u0010\u0016\u001a\u0004\b\u0014\u0010\u0015R\u0013\u0010\u0007\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\f\u00a8\u0006$"}, d2 = {"Lcom/kiriai/kiriorganization/data/models/ChatResponse;", "", "success", "", "message", "", "conversationId", "title", "_requestsUsed", "_requestsRemaining", "(Ljava/lang/Boolean;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", "getConversationId", "()Ljava/lang/String;", "getMessage", "requestsRemaining", "getRequestsRemaining", "requestsUsed", "", "getRequestsUsed", "()I", "getSuccess", "()Ljava/lang/Boolean;", "Ljava/lang/Boolean;", "getTitle", "component1", "component2", "component3", "component4", "component5", "component6", "copy", "(Ljava/lang/Boolean;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)Lcom/kiriai/kiriorganization/data/models/ChatResponse;", "equals", "other", "hashCode", "toString", "app_release"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\r\n\u0002\u0010\b\n\u0002\b\u0014\b\u0087\b\u0018\u00002\u00020\u0001BY\u0012\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u0003\u0012\n\b\u0002\u0010\u0004\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\u0007\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\b\u001a\u0004\u0018\u00010\u0005\u0012\n\b\u0002\u0010\t\u001a\u0004\u0018\u00010\u0001\u0012\n\b\u0002\u0010\n\u001a\u0004\u0018\u00010\u0001\u00a2\u0006\u0002\u0010\u000bJ\u0010\u0010\u001a\u001a\u0004\u0018\u00010\u0003H\u00c6\u0003\u00a2\u0006\u0002\u0010\u0017J\u000b\u0010\u001b\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001c\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001d\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001e\u001a\u0004\u0018\u00010\u0005H\u00c6\u0003J\u000b\u0010\u001f\u001a\u0004\u0018\u00010\u0001H\u00c2\u0003J\u000b\u0010 \u001a\u0004\u0018\u00010\u0001H\u00c2\u0003Jb\u0010!\u001a\u00020\u00002\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u00032\n\b\u0002\u0010\u0004\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\u0007\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\b\u001a\u0004\u0018\u00010\u00052\n\b\u0002\u0010\t\u001a\u0004\u0018\u00010\u00012\n\b\u0002\u0010\n\u001a\u0004\u0018\u00010\u0001H\u00c6\u0001\u00a2\u0006\u0002\u0010\"J\u0013\u0010#\u001a\u00020\u00032\b\u0010$\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010%\u001a\u00020\u0013H\u00d6\u0001J\t\u0010&\u001a\u00020\u0005H\u00d6\u0001R\u0012\u0010\n\u001a\u0004\u0018\u00010\u00018\u0002X\u0083\u0004\u00a2\u0006\u0002\n\u0000R\u0012\u0010\t\u001a\u0004\u0018\u00010\u00018\u0002X\u0083\u0004\u00a2\u0006\u0002\n\u0000R\u0013\u0010\u0006\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\rR\u0013\u0010\u0004\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\rR\u0013\u0010\b\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\rR\u0011\u0010\u0010\u001a\u00020\u00058F\u00a2\u0006\u0006\u001a\u0004\b\u0011\u0010\rR\u0011\u0010\u0012\u001a\u00020\u00138F\u00a2\u0006\u0006\u001a\u0004\b\u0014\u0010\u0015R\u0015\u0010\u0002\u001a\u0004\u0018\u00010\u0003\u00a2\u0006\n\n\u0002\u0010\u0018\u001a\u0004\b\u0016\u0010\u0017R\u0013\u0010\u0007\u001a\u0004\u0018\u00010\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\r\u00a8\u0006\'"}, d2 = {"Lcom/kiriai/kiriorganization/data/models/ChatResponse;", "", "success", "", "message", "", "conversationId", "title", "model", "_requestsUsed", "_requestsRemaining", "(Ljava/lang/Boolean;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", "getConversationId", "()Ljava/lang/String;", "getMessage", "getModel", "requestsRemaining", "getRequestsRemaining", "requestsUsed", "", "getRequestsUsed", "()I", "getSuccess", "()Ljava/lang/Boolean;", "Ljava/lang/Boolean;", "getTitle", "component1", "component2", "component3", "component4", "component5", "component6", "component7", "copy", "(Ljava/lang/Boolean;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)Lcom/kiriai/kiriorganization/data/models/ChatResponse;", "equals", "other", "hashCode", "toString", "app_release"})
 public final class ChatResponse {
     @org.jetbrains.annotations.Nullable()
     private final java.lang.Boolean success = null;
@@ -10,6 +10,8 @@ public final class ChatResponse {
     private final java.lang.String conversationId = null;
     @org.jetbrains.annotations.Nullable()
     private final java.lang.String title = null;
+    @org.jetbrains.annotations.Nullable()
+    private final java.lang.String model = null;
     @com.google.gson.annotations.SerializedName(value = "requestsUsed")
     @org.jetbrains.annotations.Nullable()
     private final java.lang.Object _requestsUsed = null;
@@ -22,6 +24,7 @@ public final class ChatResponse {
     java.lang.String message, @org.jetbrains.annotations.Nullable()
     java.lang.String conversationId, @org.jetbrains.annotations.Nullable()
     java.lang.String title, @org.jetbrains.annotations.Nullable()
+    java.lang.String model, @org.jetbrains.annotations.Nullable()
     java.lang.Object _requestsUsed, @org.jetbrains.annotations.Nullable()
     java.lang.Object _requestsRemaining) {
         super();
@@ -44,6 +47,11 @@ public final class ChatResponse {
     
     @org.jetbrains.annotations.Nullable()
     public final java.lang.String getTitle() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getModel() {
         return null;
     }
     
@@ -80,11 +88,16 @@ public final class ChatResponse {
         return null;
     }
     
-    private final java.lang.Object component5() {
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String component5() {
         return null;
     }
     
     private final java.lang.Object component6() {
+        return null;
+    }
+    
+    private final java.lang.Object component7() {
         return null;
     }
     
@@ -94,6 +107,7 @@ public final class ChatResponse {
     java.lang.String message, @org.jetbrains.annotations.Nullable()
     java.lang.String conversationId, @org.jetbrains.annotations.Nullable()
     java.lang.String title, @org.jetbrains.annotations.Nullable()
+    java.lang.String model, @org.jetbrains.annotations.Nullable()
     java.lang.Object _requestsUsed, @org.jetbrains.annotations.Nullable()
     java.lang.Object _requestsRemaining) {
         return null;
