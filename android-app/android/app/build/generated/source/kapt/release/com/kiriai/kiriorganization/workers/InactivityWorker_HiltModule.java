fa88f4ae9e9dd08f9b320c4abfa01b7d4647977c
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-24 22:58:20 +0530

    Create InactivityWorker_HiltModule.java

---
+++ android-app/android/app/build/generated/source/kapt/release/com/kiriai/kiriorganization/workers/InactivityWorker_HiltModule.java
@@ -0,0 +1,25 @@
+package com.kiriai.kiriorganization.workers;
+
+import androidx.hilt.work.WorkerAssistedFactory;
+import androidx.work.ListenableWorker;
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.StringKey;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.hilt.AndroidXHiltProcessor")
+@Module
+@InstallIn(SingletonComponent.class)
+@OriginatingElement(
+    topLevelClass = InactivityWorker.class
+)
+public interface InactivityWorker_HiltModule {
+  @Binds
+  @IntoMap
+  @StringKey("com.kiriai.kiriorganization.workers.InactivityWorker")
+  WorkerAssistedFactory<? extends ListenableWorker> bind(InactivityWorker_AssistedFactory factory);
+}
