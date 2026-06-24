c42273efda0c7d6bfb7219988eb2be0e3c63157b
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-24 17:25:16 +0530

    Update MainActivity.kt

--- android-app/android/app/src/main/java/com/kiriai/kiriorganization/MainActivity.kt
+++ android-app/android/app/src/main/java/com/kiriai/kiriorganization/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import android.content.ClipboardManager
